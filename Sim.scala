8169e1991f4450e26e07785198ec2a547c68e857
etnocigiul <etnocigiul@gmail.com>
2017-06-30 15:32:26 +0200

    Update Sim.scala

--- Sim.scala
+++ Sim.scala
@@ -1,9 +1,7 @@
 import java.util.concurrent.TimeUnit.NANOSECONDS
-import java.text.{NumberFormat, DecimalFormat}
 import scala.collection.mutable
 import scala.math
 import org.apache.spark.rdd.RDD
-import org.apache.spark.HashPartitioner 
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
@@ -40,7 +38,7 @@ object Sim extends Serializable{
 		val cartesian_autohors: RDD[((Long, Array[Long]), (Long, Array[Long]))] = authors.cartesian(authors)
 
 		cartesian_autohors.checkpoint() //eseguo il checkpointing dell'RDD cartesian_authors
-		val nAut = authors.count //authors Rdd evaluated by calling count() action
+		val nSim = cartesian_autohors.count //authors Rdd evaluated by calling count() action
 				
 		//Definisco i valori da passare alla funzione obtainSimilaritiesMatrix
 		val default_jaccard:Double = nAut / (authors.map(x => x._2.length).sum).toDouble //oppure 1 / (authors.map(x => x._2.length).sum).toDouble???
@@ -49,10 +47,10 @@ object Sim extends Serializable{
 		
 		//Salvo la matrice di similarità su file
 		val outFile = similarities_matrix.map(x => x._1 + "\t" + x._2 + "\t" + x._3)
-		outFileS.saveAsTextFile("output/sim_nID-" + nAut) 
+		outFile.coalesce(1).saveAsTextFile("output/sim_nID-" + nAut) 
 
 		val elapsedTime = System.nanoTime() - startTime
-		println("Tempo stimato di esecuzione: " + (elapsedTime / 1000000000.0) + " secondi, n° similarities: " + aut.count)
+		println("-----------------------------------\nTempo stimato di esecuzione: " + (elapsedTime / 1000000000.0) + " secondi, n° similarities: " + nSim + "\n-----------------------------------")
 		}
 
 	/*
@@ -60,7 +58,7 @@ object Sim extends Serializable{
 	* param authors: RDD contenente tutte le possibili coppie possibili di autori
 	* param default_preference: valore di default della preference
 	* param default_jaccard: valore di default per l'indice di jaccard
-	* retval: RDD contenente la matrice delle similarities
+	* retval: RDD contenente la matrice delle similarities ordinata per autori crescenti
 	*/
 	def obtainSimilaritiesMatrix(authors: RDD[( (Long, Array[Long]), (Long, Array[Long]) )], default_preference: Double, default_jaccard: Double)  : RDD[(Long, Long, Double)]  = {
 			
@@ -83,6 +81,6 @@ object Sim extends Serializable{
 			val tmp2 = initial_similarities_matrix.filter(x => x._1 != x._2)
 			
 			//Il valore di ritorno
-			tmp1.union(tmp2)
+			tmp1.union(tmp2).sortBy(x => (x._1, x._2), ascending = true)
 	}
 }
